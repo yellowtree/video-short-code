fa004be36511158e6fe8fd4ab6aceda38c20b5ff
Matt <caijiamx@gmail.com>
2014-10-08 21:41:32 +0800

    add yyt shortcode

--- video.php
+++ video.php
@@ -4,26 +4,48 @@
  * Plugin URI: https://github.com/caijiamx/video-short-code
  * Description: This plugin can  insert video quikly width shortcode ,specially for some chinese video sites, like  youku.com,tudou.com,ku6.com.
  * Author: Matt
- * Version: 1.1
+ * Version: 1.2.0
  * Author URI: http://www.xbc.me
 */
 
-define('VIDEO_SHORT_CODE_PLUGIN_DIR', plugin_dir_path( __FILE__ ));
-define('VIDEO_SHORT_CODE_PLUGIN_URL', plugin_dir_url( __FILE__ ));
-
 class VideoShortCode{
-    public $debug        = false;
+    public  $debug       = false;
     private $_optionKey  = 'video_short_code_notices';
     private $_textDomain = 'video_short_code';
     private $_logFile    = 'video_short_code.log';
     private $_object = array(
-        'youku'   => 'http://player.youku.com/player.php/sid/{code}/v.swf',
-        'tudou'   => 'http://www.tudou.com/a/{code}/v.swf',
-        'ku6'     => 'http://player.ku6.com/refer/{code}/v.swf',
-        'tvsohu'  => 'http://share.vrs.sohu.com/{code}/v.swf&topBar=1&autoplay=false&pub_catecode=0&from=page',
-        'vqq'     => 'http://static.video.qq.com/TPout.swf?vid={code}&auto=0',
-        'letv'    => 'http://i7.imgs.letv.com/player/swfPlayer.swf?id={code}&autoplay=0',
-        '56com'   => 'http://player.56.com/v_{code}.swf',
+        'youku'   => array(
+            'url' => '<iframe height="{height}" width="{width}" src="http://player.youku.com/embed/{code}" frameborder=0 allowfullscreen></iframe>',
+            'player_type'  => 'common',
+        ),
+        'tudou'   => array(
+            'url' => '<object width="{width}" height="{height}" type="application/x-shockwave-flash" data="http://www.tudou.com/a/{code}/v.swf"><param name="quality" value="high"><param name="allowScriptAccess" value="always"><param name="flashvars" value="playMovie=true&isAutoPlay=true"></object>',
+            'player_type'  => 'common',
+        ),
+        'ku6'     => array(
+            'url' => '<object width="{width}" height="{height}" type="application/x-shockwave-flash" data="http://player.ku6.com/refer/{code}/v.swf"><param name="quality" value="high"><param name="allowScriptAccess" value="always"><param name="flashvars" value="playMovie=true&isAutoPlay=true"></object>',
+            'player_type'  => 'object',
+        ),
+        'tvsohu'  => array(
+            'url' => '<object width="{width}" height="{height}" type="application/x-shockwave-flash" data="http://share.vrs.sohu.com/{code}/v.swf&topBar=1&autoplay=false&pub_catecode=0&from=page"><param name="quality" value="high"><param name="allowScriptAccess" value="always"><param name="flashvars" value="playMovie=true&isAutoPlay=true"></object>',
+            'player_type'  => 'object',
+        ),
+        'vqq'     => array(
+            'url' => '<object width="{width}" height="{height}" type="application/x-shockwave-flash" data="http://static.video.qq.com/TPout.swf?vid={code}&auto=0"><param name="quality" value="high"><param name="allowScriptAccess" value="always"><param name="flashvars" value="playMovie=true&isAutoPlay=true"></object>',
+            'player_type'  => 'object',
+        ),
+        'letv'    => array(
+            'url' => '<object width="{width}" height="{height}" type="application/x-shockwave-flash" data="http://i7.imgs.letv.com/player/swfPlayer.swf?id={code}&autoplay=0"><param name="quality" value="high"><param name="allowScriptAccess" value="always"><param name="flashvars" value="playMovie=true&isAutoPlay=true"></object>',
+            'player_type'  => 'object',
+        ),
+        '56com'   => array(
+            'url' => '<object width="{width}" height="{height}" type="application/x-shockwave-flash" data="http://player.56.com/v_{code}.swf"><param name="quality" value="high"><param name="allowScriptAccess" value="always"><param name="flashvars" value="playMovie=true&isAutoPlay=true"></object>',
+            'player_type'  => 'object',
+        ),
+        'yyt'   => array(
+            'url' => '<div style="width:{width};height:{height};"><embed pluginspage="http://www.macromedia.com/go/getflashplayer" type="application/x-shockwave-flash" width="{width}" height="{height}" flashvars="local=true&amp;amovid=5f4ffbc12418024&amp;refererdomain=yinyuetai.com&amp;domain=yinyuetai.com&amp;videoId={code}&amp;showlyrics=false&amp;capturevideoavailable=true&amp;sendsnaplog=true&amp;usepromptbar=true&amp;popupwin=true&amp;markerlocation=http%3A%2F%2Fs.yytcdn.com%2Fswf%2Fcommon%2Fmarker.%245234b9.swf&amp;preamovid=true&amp;showadvipbutton=true&amp;swflocation=http%3A%2F%2Fs.yytcdn.com%2Fswf%2Fcommon%2Fmvplayer.%2467fa6a.swf" bgcolor="#000000" allowfullscreen="true" allowscriptaccess="always" wmode="window" id="yinyuetaiplayer" name="yinyuetaiplayer" src="http://s.yytcdn.com/swf/common/playerloader.$f92adc.swf"></div>',
+            'player_type'  => 'embed',
+        ),
     );
     private $_regexs = array(
         'youku'   => array(
@@ -31,16 +53,16 @@ class VideoShortCode{
             'count' => 1,
         ),
         'tudou'   => array(
-            'regex' => '/http:\/\/www\.tudou\.com\/listplay\/(\w+)\/(\w+)\.html/',
-            'count' => 1,
+            'regex' => '/http:\/\/www\.tudou\.com\/(listplay|albumplay)\/(\w+)\/(\w+)\.html/',
+            'count' => 3,
         ),
         'ku6'     => array(
             'regex' => '/http:\/\/v\.ku6\.com\/show\/([-\w]+\.\.)\.html(\?hpsrc=(\w+))?/',
             'count' => 1,
         ),
         'tvsohu'  => array(
-            'regex' => '',
-            'count' => 0,
+            /*'regex' => '/http:\/\/tv\.sohu\.com\/(\w+)\/(\w+)\.shtml/',
+            'count' => 2,*/
         ),
         'vqq'     => array(
             'regex' => '/http:\/\/v\.qq\.com\/cover\/(\w+)\/(\w+)\.html\?vid=(\w+)/',
@@ -53,6 +75,10 @@ class VideoShortCode{
         '56com'   => array(
             'regex' => '/http:\/\/www\.56\.com\/(\w+)\/v_(\w+)\.html/',
             'count' => 2,
+        ),
+        'yyt'   => array(
+            'regex' => '/http:\/\/v\.yinyuetai\.com\/video\/(\d+)/',
+            'count' => 1,
         ),
     );
 
@@ -78,7 +104,10 @@ class VideoShortCode{
     public function run(){
         $result = $this->checkRequire();
         if($result){
-            $this->_logFile    = VIDEO_SHORT_CODE_PLUGIN_DIR . $this->_logFile;
+            //初始化相关插件信息
+            $plugin_dir        = plugin_dir_path( __FILE__ );
+            $plugin_url        = plugin_dir_url( __FILE__ );
+            $this->_logFile    = $plugin_dir . $this->_logFile;
             $this->loger('$this->_logFile = ' . $this->_logFile , __FUNCTION__);
             add_shortcode('youku', array($this , 'play_youku'));
             add_shortcode('tudou', array($this , 'play_tudou'));
@@ -87,6 +116,7 @@ class VideoShortCode{
             add_shortcode('vqq', array($this , 'play_vqq'));
             add_shortcode('letv', array($this , 'play_letv'));
             add_shortcode('56com', array($this , 'play_56com'));
+            add_shortcode('yyt', array($this , 'play_yyt'));
             add_filter( 'content_save_pre', array($this , 'saveContentBefore'), 10, 1 );
         }
     }
@@ -123,6 +153,10 @@ class VideoShortCode{
         return $this->_call(__FUNCTION__ , $atts);
     }
 
+    public function play_yyt($atts){
+        return $this->_call(__FUNCTION__ , $atts);
+    }
+
     private function _call($name, $atts){
         $type = str_replace('play_', '', $name);
         $atts = shortcode_atts(array(
@@ -133,14 +167,16 @@ class VideoShortCode{
         $code   = $atts['code'];
         $width  = $atts['width'];
         $height = $atts['height'];
-        $data   = $this->_object[$type];
-        $data   = str_replace('{code}', $code, $data);
-        $flash = '<object width="'.$width.'" height="'.$height.'" type="application/x-shockwave-flash" data="' . $data .'"><param name="quality" value="high"><param name="allowScriptAccess" value="always"><param name="flashvars" value="playMovie=true&isAutoPlay=true"></object>';
+        $template = $this->_object[$type]['url'];
+        $data     = str_replace('{code}', $code, $template);
+        $flash    = str_replace('{width}' , $width , $data);
+        $flash    = str_replace('{height}' , $height , $flash);
         $this->loger('$name = ' . $name , __FUNCTION__);
         $this->loger($atts , __FUNCTION__);
         $this->loger('$type = ' . $type , __FUNCTION__);
         $this->loger('$code = ' . $code , __FUNCTION__);
         $this->loger('$data = ' . $data , __FUNCTION__);
+        $this->loger('$flash = ' . $flash , __FUNCTION__);
         if(is_single()){
             return $flash ;
         }
@@ -152,7 +188,8 @@ class VideoShortCode{
             $regex = $value['regex'];
             if($value && $regex){
                 $count = $value['count'];
-                $replace = '[' . $key . " code='\${" . $count . "}']";
+                $format  = '[%s code=\'${%d}\']';
+                $replace = sprintf($format , $key , $count);
                 $content = preg_replace( $regex, $replace , $content);
                 $this->loger('$regex = ' . $regex , __FUNCTION__);
                 $this->loger('$replace = ' . $replace , __FUNCTION__);
